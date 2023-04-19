a2bffbdde179c51761c413897a676071d54f5de5
DmytroSyrotenko <dmitriysirotenko90@gmail.com>
2023-04-19 15:19:59 +0300

    created

---
+++ src/ua/syrotenko/hw6/Phone.java
@@ -0,0 +1,62 @@
+package ua.syrotenko.hw6;
+/*Класс Phone.
+    а) Создайте класс Phone, который содержит переменные(поля) number,
+    model и weight.
+    б) Добавить конструктор в класс Phone, который принимает на вход
+    три параметра для инициализации переменных класса - number, model
+    и weight.
+    в) Добавить конструктор, который принимает на вход два параметра
+    для инициализации переменных класса - number, model.
+    г) Добавить конструктор без параметров.
+    д) Создайте три экземпляра этого класса.
+    ж) Присвоить значения полям класса.
+    з) Выведите на консоль значения их полей.
+    и) Добавить в класс Phone методы: receiveCall, имеет один параметр
+    – имя звонящего. Выводит на консоль сообщение “Звонит {name}”.
+    getNumber – возвращает номер телефона. Вызвать эти методы для
+    каждого из объектов.
+     */
+
+public class Phone {
+
+    String number;
+    String model;
+    double weight;
+
+
+    public void receiveCall(String name) {
+        System.out.println("Звонит " + name);
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public Phone(String number, String model, int weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+
+    }
+
+    public Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+
+    }
+
+    public Phone() {
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "number=" + number +
+                ", model='" + model + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
---
+++ src/ua/syrotenko/hw6/Task1.java
@@ -0,0 +1,44 @@
+package ua.syrotenko.hw6;
+
+import java.awt.*;
+
+public class Task1 {
+    /*Класс Phone.
+    а) Создайте класс Phone, который содержит переменные(поля) number,
+    model и weight.
+    б) Добавить конструктор в класс Phone, который принимает на вход
+    три параметра для инициализации переменных класса - number, model
+    и weight.
+    в) Добавить конструктор, который принимает на вход два параметра
+    для инициализации переменных класса - number, model.
+    г) Добавить конструктор без параметров.
+    д) Создайт'е три экземпляра этого класса.
+    ж) Присвоить значения полям класса.
+    з) Выведите на консоль значения их полей.'
+    и) Добавить в класс Phone методы: receiveCall, имеет один параметр
+    – имя звонящего. Выводит на консоль сообщение “Звонит {name}”.
+    getNumber – возвращает номер телефона. Вызвать эти методы для
+    каждого из объектов.
+     */
+    public static void main(String[] args) {
+
+        Phone nokia3310 = new Phone("0502845612", "Nokia 3310", 665);
+        Phone iPhone12 = new Phone("0984561245", "iPhone 12");
+        Phone lenovo = new Phone();
+
+
+        System.out.println(nokia3310);
+        System.out.println(iPhone12);
+        System.out.println(lenovo);
+
+
+        nokia3310.receiveCall("Alex");
+        System.out.println(nokia3310.getNumber());
+
+        iPhone12.receiveCall("Richard");
+        System.out.println(iPhone12.getNumber());
+
+        lenovo.receiveCall("James");
+        System.out.println(lenovo.getNumber());
+    }
+}
